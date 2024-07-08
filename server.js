e5ce0d7498840d4a5549ab93c87ff07067ef6d99
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-08 15:16:27 +0000

    Add solution for authentication

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ import config from './config/config.js'
 import app from './server/express.js'
 import mongoose from 'mongoose' 
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true,
+mongoose.connect(config.mongoUri, { 
+    //useNewUrlParser: true,
 //useCreateIndex: true, 
-useUnifiedTopology: true } )
+//useUnifiedTopology: true
+ } )
 
- .then(() => {
-   console.log("Connected to the database!");
-   })
-  
+.then(() => {
+    console.log("Connected to the database!");
+    })
+    
 mongoose.connection.on('error', () => {
 throw new Error(`unable to connect to database: ${config.mongoUri}`) 
 })
